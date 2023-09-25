83cf851c762d807d5f4c32da059217e55d7debb3
Jesse Himmelstein <jesse@heavenstone.net>
2023-09-25 11:50:30 +0200

    Handles multi-pack

--- src/index.js
+++ src/index.js
@@ -9,12 +9,28 @@ module.exports = new Transformer({
     const parsed = JSON.parse(source);
 
     // Replace the filename in meta.image with a dependency reference
-    const imageDep = parsed.meta.image;
-    const dependencyId = asset.addURLDependency(imageDep);
-    parsed.meta.image = dependencyId;
-    logger.verbose({
-      message: "Added dependency for " + imageDep + " id " + dependencyId,
-    });
+    {
+      const imageDep = parsed.meta.image;
+      const dependencyId = asset.addURLDependency(imageDep);
+      parsed.meta.image = dependencyId;
+      logger.verbose({
+        message: "Added dependency for " + imageDep + " id " + dependencyId,
+      });
+    }
+
+    // Replace the filename in meta.related_multi_packs with dependency references
+    if (parsed.meta.related_multi_packs) {
+      parsed.meta.related_multi_packs = parsed.meta.related_multi_packs.map(
+        (spritesheetName) => {
+          const dependencyId = asset.addURLDependency(spritesheetName);
+          logger.verbose({
+            message:
+              "Added dependency for " + spritesheetName + " id " + dependencyId,
+          });
+          return dependencyId;
+        }
+      );
+    }
 
     // Update the asset
     asset.setCode(JSON.stringify(parsed, null, 2));
